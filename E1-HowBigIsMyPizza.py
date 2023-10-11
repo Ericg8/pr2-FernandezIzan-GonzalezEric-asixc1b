84daceb51142f1ceafc8209dc09661fd7e96e7c2
eric.gonzalez.7e7 <eric.gonzalez.7e7@itb.cat>
2023-10-11 12:15:52 +0200

    Ejercicio1

---
+++ E1-HowBigIsMyPizza.py
@@ -0,0 +1,13 @@
+"""
+Eric González, Izan Fernandez
+11/10/2023
+M03 UF1 A1
+Descripció: Activitat 2
+
+"""
+import math
+
+diametre = int(input("Escriu el diametre de la pizza: "))
+radi = diametre / 2
+Area = math.pi * (radi**2)
+print("La superficie de la pizza es de",Area)
--- main.py
+++
@@ -1,7 +0,0 @@
-"""
-Eric González Casado
-11/10/2023
-M03 UF1 A1
-Descripció: Activitat 2
-
-"""
